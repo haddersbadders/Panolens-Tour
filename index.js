f435a03f2598d3e6b90938181f7e2c918d183eb5
Hadrian Cawthorne <h.p.cawthorne@sheffield.ac.uk>
2019-12-13 15:03:01 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-var panorama, panorama2, viewer, container, infospot;
+var panorama, panorama2, viewer, container, infospot1;
 
 container = document.querySelector( '#container' );
-
+infospot1 = new PANOLENS.Infospot();
+      infospot1.position.set( -4732.52, -1372.88, -786.34 );
+      infospot1.addHoverText( 'This is the sandpit' );
 
  panorama = new PANOLENS.ImagePanorama( 'images/broomhall/broomhall_kitchen_2.jpg' );
- viewer = new PANOLENS.Viewer( { container: container });
+ viewer = new PANOLENS.Viewer( { container: container, output: 'console' });
+ panorama.add( infospot1 );
+
 viewer.add( panorama );
